e34d853c65a478e58cce64df59e8ef27cedad125
meher09 <mehernigarcu@gmail.com>
2022-10-25 20:47:03 +0600

    react icon added and why choose us section done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Carousel from './Carousel';
+import WhyChooseUs from './WhyChooseUs';
 
 const Home = () => {
     return (
         <>
             <Carousel></Carousel>
+            <WhyChooseUs></WhyChooseUs>
         </>
     );
 };
---
+++ src/components/WhyChooseUs.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { HiBadgeCheck, HiLightningBolt, HiArrowSmDown } from "react-icons/hi"
+const WhyChooseUs = () => {
+    return (
+        <div className="container my-5">
+            <h2 className='text-uppercase mb-3'>Why should you choose us</h2>
+            <hr className='w-50 mx-auto mb-5' />
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="card py-5">
+                        <div className="card-body">
+                            <HiBadgeCheck className='h1 text-success' />
+                            <h5 className="card-title">We Care you</h5>
+                            <p className="card-text">We take care of every students.
+                                Dedicated support panel who are regularly monitors every students progress</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="card py-5">
+                        <div className="card-body">
+                            <HiLightningBolt className="h1 text-success" />
+                            <h5 className="card-title">Assignment & Quizes</h5>
+                            <p className="card-text">Our dedicated team take care of the students progress. We regularly take asesment and quizes for you which will help tracking your progress</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
+
+    );
+};
+export default WhyChooseUs;
