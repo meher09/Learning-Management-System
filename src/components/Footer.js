2446959c7abf6eccb8e6c47f182bbd5f40139e90
meher09 <mehernigarcu@gmail.com>
2022-10-27 16:13:28 +0600

    Added Footer at the bottom of the page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="mt-auto">
+        <footer className="mt-auto my-footer">
         <div className='bg-dark text-white text-center container-fluid py-5 my-3'>
             Copyright, 2022, Itshikkha
         </div>
