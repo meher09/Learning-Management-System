7de3b7d3cdc901a390a26b3fa7329fd38b2c8a7e
meher09 <mehernigarcu@gmail.com>
2022-10-27 22:23:38 +0600

    Finalized the checkout page

--- src/components/CheckOut.js
+++ src/components/CheckOut.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthProvider/AuthProvider';
 
 const CheckOut = () => {
-
-
     const course = useLoaderData()
-
     const total_fee = course.fee.course_fee - course.fee.discount
+    const { user } = useContext(AuthContext)
+    console.log(user.displayName !== null)
 
-    console.log(course)
     return (
 
-        <div className='container'>
+        <div className='container text-center'>
             <div className="row my-5">
                 <div className="alert alert-success" role="alert">
                     You have successfully enrolled the course
@@ -19,16 +18,24 @@ const CheckOut = () => {
                 <div className="col-md-6 my-5">
                     <div className="card">
                         <div className="card-body">
-                            <img src="https://randomuser.me/api/portraits/men/27.jpg" className='rounded-circle' alt="" />
+                            <img src={user.photoURL} className='rounded-circle' alt="" height="160" />
                             <h2 className="card-title h4 my-3">Your Profile Information</h2>
 
-                            <div className="d-flex justify-content-between">
-                                <p className='text-start'>Full Name:</p><p className='text-end'>Your Name Here</p>
-                            </div>
-                            <div className="d-flex justify-content-between">
-                                <p className='text-start'>Email:</p><p className='text-end'>200 USD</p>
-                            </div>
+                            {
+                                user.displayName !== null && <div className="d-flex justify-content-between">
+                                    <p className='text-start'>Full Name:</p><p className='text-end'>{user.displayName}</p>
+                                </div>
 
+                            }
+                            {
+                                user.displayName !== null &&
+
+                                <div className="d-flex justify-content-between">
+                                    <p className='text-start'>Email:</p><p className='text-end'>{user.email}</p>
+                                </div>
+                            }
+
+                            {user.displayName === null && <p className='text-center text-danger'>Github doesn't share your information</p>}
                         </div>
                     </div>
 
